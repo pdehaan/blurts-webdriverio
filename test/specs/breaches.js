4f70c3e31d35e82914823792eb94e5dae864b59a
Peter deHaan <peter@deseloper.com>
2018-07-03 16:07:24 -0700

    Tweaks to breaches.json

--- test/specs/breaches.js
+++ test/specs/breaches.js
@@ -1,28 +1,26 @@
 const assert = require("assert");
 
-const breaches = require("../../breaches.json");
+const breaches = require("blurts-addon/src/breaches.json");
 
 describe("Firefox Monitor", () => {
+  browser.setViewportSize({ width: 1024, height: 900 });
+
   it("should have the right title", () => {
     browser.url("/");
     assert.equal(browser.getTitle(), "Firefox Monitor");
   });
 
+  breaches.forEach(breach => {
   // Take screenshots of each of the details page for each breach...
-  it("should show breach details", () => {
-    browser.setViewportSize({ width: 1024, height: 900 });
-
-    breaches.forEach(breach => {
-      const [slug] = breach.LogoFilename.split(".");
-      const breachUri = `/?breach=${slug}`;
-
-      // console.log(`Scraping ${breachUri}...`);
+  it(`should show breach details for ${breach.Name}`, () => {
+      const breachUri = `/?breach=${breach.Name}`;
       browser.url(breachUri);
-      // Take a full page screenshot...
-      // browser.saveScreenshot(`./test/shots/${slug}.png`);
 
       // Take a screenshot of the selected element...
-      browser.saveElementScreenshot(`test/shots/${slug}.png`, ".account");
+      browser.saveElementScreenshot(`test/shots/${breach.Name}.png`, ".account");
+
+      // Take a full page screenshot...
+      // browser.saveScreenshot(`./test/shots/${breach.Name}.png`);
     });
   });
 });
